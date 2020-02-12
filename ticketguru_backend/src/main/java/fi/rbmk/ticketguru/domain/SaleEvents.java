d6e2b7a75598963edc5adb738aaa7c230cdc41d1
VarjoDesigns <57039872+VarjoDesigns@users.noreply.github.com>
2020-02-12 23:00:49 +0200

    Lisätty repot + korjattu olioita
    SaleEvents (korjattu)
    SaleEventsRepository (Lisätty)
    SaleRows (korjattu)
    SaleRowsRepository (Lisätty)
    PostCodes (korjattu)
    PostCodesRepository (Lisätty)

--- ticketguru_backend/src/main/java/fi/rbmk/ticketguru/domain/SaleEvents.java
+++ ticketguru_backend/src/main/java/fi/rbmk/ticketguru/domain/SaleEvents.java
@@ -2,13 +2,19 @@ package fi.rbmk.ticketguru.domain;
 
 import java.util.Date;
 import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 
 @Entity
 public class SaleEvents {
 	
 	@Id
-	@GeneratedValue(strategy=generationType.AUTO)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long saleEvent_ID;
 	private Date saleEventDateTime;
 	private Long user_ID;
@@ -27,28 +33,40 @@ public class SaleEvents {
 		this.user_ID = user_ID;
 	}
 
+	// Getterit
+
 	public Long getSaleEvent_ID() {
 		return saleEvent_ID;
-	}
-
-	public void setSaleEvent_ID(Long saleEvent_ID) {
-		this.saleEvent_ID = saleEvent_ID;
 	}
 
 	public Date getSaleEventDateTime() {
 		return saleEventDateTime;
 	}
 
-	public void setSaleEventDateTime(Date saleEventDateTime) {
-		this.saleEventDateTime = saleEventDateTime;
-	}
-
 	public Long getUser_ID() {
 		return user_ID;
 	}
 
+	public List<SaleRows> getSaleRows() {
+		return saleRows;
+	}
+	
+	// Setterit
+	
+	public void setSaleEvent_ID(Long saleEvent_ID) {
+		this.saleEvent_ID = saleEvent_ID;
+	}
+
+	public void setSaleEventDateTime(Date saleEventDateTime) {
+		this.saleEventDateTime = saleEventDateTime;
+	}
+
 	public void setUser_ID(Long user_ID) {
 		this.user_ID = user_ID;
+	}
+
+	public void setSaleRows(List<SaleRows> saleRows) {
+		this.saleRows = saleRows;
 	}
 
 	@Override
