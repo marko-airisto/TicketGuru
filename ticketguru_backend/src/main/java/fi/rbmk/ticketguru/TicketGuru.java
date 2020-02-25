059b4a97781a5dfedec2bdfce7cdfa9833bc7a40
LastHouse <54175608+LastHouse@users.noreply.github.com>
2020-02-25 11:24:23 +0200

    Application fix

--- ticketguru_backend/src/main/java/fi/rbmk/ticketguru/TicketGuru.java
+++ ticketguru_backend/src/main/java/fi/rbmk/ticketguru/TicketGuru.java
@@ -2,25 +2,12 @@ package fi.rbmk.ticketguru;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class TicketGuru {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketGuru.class, args);
-	}
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:3000");
-			}
-		};
 	}
 
 }
