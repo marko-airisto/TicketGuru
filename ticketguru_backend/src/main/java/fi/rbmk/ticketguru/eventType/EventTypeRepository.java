d65c1d4d3319c17c163cb3e894c6813ffbbacb22
malasip <alasippolamika@gmail.com>
2020-03-03 23:00:31 +0200

    Fixes

--- ticketguru_backend/src/main/java/fi/rbmk/ticketguru/eventType/EventTypeRepository.java
+++ ticketguru_backend/src/main/java/fi/rbmk/ticketguru/eventType/EventTypeRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 @RepositoryRestResource
 public interface EventTypeRepository extends CrudRepository<EventType, Long> {
 
-	List<EventType> findByEventTypes(String name );
+	List<EventType> findByName(String name );
 	
 	List<EventType> findAll();
 	// List<EventOrganizer> findByEventName(String eventName);
