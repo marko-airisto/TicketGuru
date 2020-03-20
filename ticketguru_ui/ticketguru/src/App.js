e454a7a6783d63f8d93aa374168c8e00318c5d3b
LastHouse <54175608+LastHouse@users.noreply.github.com>
2020-03-20 18:57:35 +0200

    adding jwt

--- ticketguru_ui/ticketguru/src/App.js
+++ ticketguru_ui/ticketguru/src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
-import './App.css';
-import Users from './components/pages/Users';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Users from './pages/Users';
+import { PrivateRoute } from './_components/PrivateRoute';
 
 function App() {
   return (
-    <div className="App">
-      <Users />
+    <div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <PrivateRoute path="/Home" component={Home} />
+          <PrivateRoute path="/Users" component={Users} />
+        </Switch>
+      </Router>
     </div>
   );
 }
