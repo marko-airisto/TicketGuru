34f1bdc9a07fe55a55b9c99640d94db32ee07cbd
malasip <alasippolamika@gmail.com>
2020-03-25 19:43:13 +0200

    Ticket service implementaatio ja demoa

--- ticketguru_backend/src/main/java/fi/rbmk/ticketguru/saleRow/SaleRow.java
+++ ticketguru_backend/src/main/java/fi/rbmk/ticketguru/saleRow/SaleRow.java
@@ -1,6 +1,8 @@
 package fi.rbmk.ticketguru.saleRow;
 
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,12 +10,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import org.springframework.hateoas.ResourceSupport;
 
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.OneToMany;
 
 import fi.rbmk.ticketguru.ticket.Ticket;
 import fi.rbmk.ticketguru.saleEvent.SaleEvent;
@@ -35,19 +39,17 @@ public class SaleRow extends ResourceSupport {
 	@JoinColumn(name = "saleEvent_ID")
 	private SaleEvent saleEvent;
 
-	@OneToOne
-	@JoinColumn(name = "ticket_ID")
-	private Ticket ticket;
+	@OneToMany(mappedBy = "saleRow")
+	private List<Ticket> tickets;
 
 	public SaleRow() {
 		super();
 	}
 
-	public SaleRow(Long id, SaleEvent saleEvent, Ticket ticket, Long discount) {
+	public SaleRow(Long id, SaleEvent saleEvent, Long discount) {
 		super();
 		this.saleRow_ID = id;
 		this.saleEvent = saleEvent;
-		this.ticket = ticket;
 		this.discount = discount;
 	}
 
@@ -60,8 +62,8 @@ public class SaleRow extends ResourceSupport {
 		return this.saleRow_ID;
 	}
 
-	public Ticket getTicket() {
-		return this.ticket;
+	public List<Ticket> getTickets() {
+		return this.tickets;
 	}
 
 	public Long getDiscount() {
@@ -71,10 +73,6 @@ public class SaleRow extends ResourceSupport {
 	// Setterit
 	public void setSaleEvent(SaleEvent saleEvent) {
 		this.saleEvent = saleEvent;
-	}
-
-	public void setTicket(Ticket ticket) {
-		this.ticket = ticket;
 	}
 
 	public void setDiscount(Long discount) {
---
+++ ticketguru_backend/src/main/java/fi/rbmk/ticketguru/ticket/TicketLinks.java
@@ -0,0 +1,35 @@
+package fi.rbmk.ticketguru.ticket;
+
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.*;
+
+import org.springframework.hateoas.Link;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class TicketLinks {
+
+    private Link selfLink, eventTicketLink, ticketStatusLink, saleRowLink;
+    private List<Link> linkList;
+
+    public TicketLinks(Ticket ticket) {
+        Long id = ticket.getTicket_ID();
+        this.selfLink = linkTo(TicketController.class).slash(id).withSelfRel();
+        this.eventTicketLink = linkTo(methodOn(TicketController.class).getEventTicket(id)).withRel("eventTicket");
+        this.ticketStatusLink = linkTo(methodOn(TicketController.class).getTicketStatus(id)).withRel("ticketStatus");
+        //this.saleRowLink = linkTo(methodOn(TicketController.class).getSaleRow(id)).withRel("saleRow");
+        this.linkList = Arrays.asList(
+            selfLink,
+            eventTicketLink,
+            ticketStatusLink
+            //saleRowLink
+        );
+    }
+
+    // Getters
+    public Link getSelfLink() { return selfLink; }
+    public Link getEventTicketLink() { return eventTicketLink; }
+    public Link getTicketStatus() { return ticketStatusLink; }
+    public Link getSaleRowLink() { return saleRowLink; }
+    public List<Link> getAll() { return linkList; }
+}
